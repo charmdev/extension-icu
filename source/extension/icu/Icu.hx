df6281005642f9c5b14cd01ad0c6ac4651bdaaad
Zaphod <zaphy@yandex.ru>
2020-07-17 23:04:50 +0400

    Update Icu.hx

--- source/extension/icu/Icu.hx
+++ source/extension/icu/Icu.hx
@@ -2,12 +2,30 @@ package extension.icu;
 
 import cpp.Lib;
 
+typedef RunInfo = {
+	var start:Int;
+	var length:Int;
+	var direction:Int;
+}
+
 class Icu 
 {
+	public static function init():Void
+    {
+        native_init();
+    }
+	
     public static function test(text:String):String
     {
         return native_test(text);
     }
-
-    private static var native_test = Lib.load ("extension-icu", "nme_icu_log2vis", 1);
+	
+	public static function getVisualRuns(text:String, ltr:Bool = true):Array<RunInfo>
+	{
+		return native_get_visual_runs(text, ltr ? 0 : 1);
+	}
+	
+    private static var native_test = Lib.load("extension-icu", "nme_icu_log2vis", 1);
+    private static var native_init = Lib.load("extension-icu", "nme_icu_init", 0);
+    private static var native_get_visual_runs = Lib.load("extension-icu", "nme_icu_get_visual_runs", 2);
 }
